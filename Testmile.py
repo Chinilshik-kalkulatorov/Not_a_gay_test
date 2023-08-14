b37ddb5a984a3d72921c4cd0bbfb0657ffce4342
Chinilshik-kalkulatorov <95532146+Chinilshik-kalkulatorov@users.noreply.github.com>
2023-08-14 12:33:41 +0500

    Update Testmile.py

--- Testmile.py
+++ Testmile.py
@@ -1,12 +1,29 @@
-username = input("Enter username: ")  # Prompt user for name
+# Sample Python Code for Student Assessment Management System
 
-if username == 'Alan':  # Check
-    print("Why are you gay?"
-          )
-    massege = "Who says I am gay?"
-    if input("") == massege:
-        print("You are gay.")
-    else:
-        print("No metter what you say. You are gay.")
-else:
-    print("You are not gay")
+class AssessmentSystem:
+    def __init__(self):
+        # Initializing user and grades databases
+        self.users = {'admin': 'password123'}
+        self.grades = {}
+
+    def login(self, username, password):
+        # Check if the username and password match the database
+        if username in self.users and self.users[username] == password:
+            return "Access granted"
+        else:
+            return "Access denied"
+
+    def view_grades(self, studentID):
+        # Retrieve grades for a specific student
+        return self.grades.get(studentID, "No grades available")
+
+    def add_edit_grades(self, studentID, assessment, grade):
+        # Add or edit grades for a student
+        if studentID not in self.grades:
+            self.grades[studentID] = {}
+        self.grades[studentID][assessment] = grade
+        return "Grade updated"
+
+    def feedback(self, studentID, message):
+        # Send feedback to a student's teacher (placeholder function)
+        return "Feedback sent to teacher"
